ae588935be146d825c8c2eaeb5fbb74ae8ca199e
Charlotte Clark <charlocc@usc.edu>
2021-08-15 20:20:33 -0700

    Added pseudocode to javascript

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-var timer 
-var 
+var timeEl = document.querySelector(".time");
+
+// WHEN the user clicks "start quiz", 
+// THEN the first question with answer choices appears
+// THEN a timer appears
+
+// WHEN the user answers a question 
+// THEN a "correct" or an "incorrect" alert appears
+// THEN the next question and answer choices appear
+
+// WHEN the user finishes the quiz,
+// THEN an alert appears with their score
+// THEN the user can input their initials (prompt)
+// THEN the user can click to see the high scores
